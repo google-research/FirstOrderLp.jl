acdfd230f7f084d0f67c8aaff7162dd531a82a1e
Miles Lubin <mlubin@google.com>
2021-05-03 10:51:28 -0400

    Add missing license headers (#42)

--- benchmarking/collect_mittelmann_benchmark.sh
+++ benchmarking/collect_mittelmann_benchmark.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # This script illustrates how to collect the instances in the "Mittelmann"
 # benchmark set. Although it can be used directly to collect the benchmark, it
 # is intended primarily as a guide to collecting the benchmark - it makes
 # several assumptions about the environment. In particular, it assumes
 # wget, tar, bzcat, gzip, zcat, and cc exist and work as expected.
-
+#
 # This collects the union of the instances from:
 # * Benchmark of Simplex LP solvers: http://plato.asu.edu/ftp/lpsimp.html
 # * Benchmark of Barrier LP solvers: http://plato.asu.edu/ftp/lpbar.html
