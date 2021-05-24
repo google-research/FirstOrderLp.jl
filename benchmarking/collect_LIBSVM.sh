11dc6611cfe03948af6deebd957f02199b113bea
Miles Lubin <mlubin@google.com>
2021-05-24 14:34:12 -0400

    Simplify collect_LIBSVM.sh (#64)
    * Simplify collect_LIBSVM.sh
    
    * update benchmarking/README.md
    
    * bash style fix

--- benchmarking/collect_LIBSVM.sh
+++ benchmarking/collect_LIBSVM.sh
@@ -18,21 +18,18 @@
 # and working.
 
 
-if [[ "$#" != 2 ]]; then
-    echo "Usage: collect_LIBSVM.sh temporary_dir output_dir" 1>&2
+if [[ "$#" != 1 ]]; then
+    echo "Usage: collect_LIBSVM.sh output_dir" 1>&2
     exit 1
 fi
 
-TEMP_DIR="$1"
-DEST_DIR="$2"
+DEST_DIR="$1"
 
-mkdir -p "${TEMP_DIR}" || exit 1
 mkdir -p "${DEST_DIR}" || exit 1
 
 # To download the binary problems:
 data_source="https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary"
 for filename in kdda.t real-sim avazu-app.val; do
-    curl "${data_source}/${filename}.bz2" --output "${TEMP_DIR}/${filename}.bz2" || exit 1
-    bunzip2 -d "${TEMP_DIR}/${filename}.bz2"
-    mv "${TEMP_DIR}/${filename}" "${DEST_DIR}/${filename}"
+    curl "${data_source}/${filename}.bz2" --output "${DEST_DIR}/${filename}.bz2" || exit 1
+    bunzip2 "${DEST_DIR}/${filename}.bz2"
 done
